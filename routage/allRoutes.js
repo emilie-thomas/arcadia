0aeb461f7889a50118e4f88900b528b46d450f3b
emilie-thomas <emiliethomas34@gmail.com>
2024-07-15 10:12:06 +0200

    Ajout du routage

---
+++ routage/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from './Route.js';
+
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+];
+
+// titre de la page : Route.titre - websiteName
+export const websiteName = "Zoo Arcadia";
